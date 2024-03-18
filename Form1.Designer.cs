714e4947ee94a03c8692556fb75c0e589be56db6
beebdoles <beebdoles132435@gmail.com>
2024-03-18 03:29:14 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Morse_Code_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            label2 = new Label();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 21);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(107, 64);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Interval = 300;
+            timer2.Tick += timer2_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(140, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 23);
+            button1.TabIndex = 2;
+            button1.Text = "start/stop timer";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 186);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(260, 21);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(109, 64);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 461);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private Button button1;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace Morse_Code_Solver
+{
+    public partial class Form1 : Form
+    {
+        private Boolean buttonState = true;
+        private Color[] listOfColours;
+        private List<string> morseLights = new List<string>();
+        private List<string> morse = new List<string>();
+        private int numOfUnits = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //takeColour();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void takeColour(int n)
+        {
+            Bitmap bitmap = new Bitmap(1, 1);
+            Rectangle bounds = new Rectangle(MousePosition.X, MousePosition.Y, 1, 1);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+                graphics.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+            Color colour = bitmap.GetPixel(0, 0);
+
+            label1.Text = colour.ToString();
+            pictureBox1.BackColor = colour;
+
+            listOfColours[n] = colour;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            takeColour(numOfUnits);
+            ++numOfUnits;
+            //pictureBox2.BackColor
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (buttonState)
+            {
+                buttonState = false;
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                numOfUnits = 0;
+                listOfColours = new Color[5000];
+                morseLights.Clear();
+                morse.Clear();
+                timer2.Enabled = true;
+            }
+            else
+            {
+                timer2.Enabled = false;
+                buttonState = true;
+                colourToPseudoMorse();
+                pseudoMorseToMorse();
+                string rep = "";
+                for (int i = 0; i < morse.Count; ++i)
+                {
+                    rep += morse[i];
+                }
+                label2.Text = rep;
+            }
+        }
+
+        private void colourToPseudoMorse()
+        {
+            for (int i = 0; i < listOfColours.Length; ++i)
+            {
+                if (listOfColours[i] != Color.Empty)
+                {
+                    Color tempColour = listOfColours[i];
+                    //set default as RGB(0, 6, 16) for black, RGB(255, 237, 59)
+
+                    double distance1 = Math.Sqrt(Math.Pow(tempColour.R, 2) + Math.Pow(tempColour.G - 6, 2) + Math.Pow(tempColour.B - 16, 2));
+                    double distance2 = Math.Sqrt(Math.Pow(tempColour.R - 255, 2) + Math.Pow(tempColour.G - 237, 2) + Math.Pow(tempColour.B - 59, 2));
+
+                    if (distance1 < distance2)
+                    {
+                        morseLights.Add("1");
+                        label3.Text += "1";
+                    }
+                    else
+                    {
+                        morseLights.Add("0");
+                        label3.Text += "0";
+                    }
+                    label4.Text += distance1 + " " + distance2 + " ";
+                }
+                else
+                {
+                    i = 10001;
+                }
+                //label2.Text += String.Join("", morseLights);
+            }
+        }
+
+        private void pseudoMorseToMorse()
+        {
+            for (int i = 0; i < morseLights.Count; ++i)
+            {
+                string symbol = morseLights[i];
+                int length = 0;
+                Boolean condition = true;
+                while (condition)
+                {
+                    if (i < morseLights.Count && morseLights[i].Equals(symbol))
+                    {
+                        ++length;
+                        ++i;
+                    }
+                    else
+                    {
+                        condition = false;
+                    }
+                }
+                if (symbol.Equals("1"))
+                {
+                    if (length == 1)
+                    {
+                        morse.Add(".");
+                    }
+                    else
+                    {
+                        morse.Add("-");
+                    }
+                }
+                else
+                {
+                    if (length == 3)
+                    {
+                        morse.Add("/");
+                    }
+                    else
+                    {
+                        morse.Add("|");
+                    }
+                }
+            }
+        }
+    }
+
+    //add a lookup table for morse code values :)
+    //(0 6 16) (4 7 13) (7 9 18)              (255 237 59) (255 221 61) (255 226 58)
+    //5.1, 7.87                                //16.1, 11
+}
